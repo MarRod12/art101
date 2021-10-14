545a40fcf4f8f898b4b990cb35a76a6184e15609
MarRod12 <91565970+MarRod12@users.noreply.github.com>
2021-10-13 20:44:25 -0700

    lab 4 done

---
+++ lab4/js/lab.js
@@ -0,0 +1,18 @@
+/**
+ * Summary.
+ *  Turn pseudocode into comments.
+ *
+ * Description.
+ *  This file holds pseudocode that was made in Google Docs and then pasted  into the javascript file.
+ *
+ * @link: file:///Users/margaritarodriguez/Desktop/art101/lab4/index.html
+ * @file
+ * @author Margarita Rodriguez and Brenda Alvarez
+ * @since
+ */
+
+//Open your computer
+//Navigate to system preferences.
+//Click “time machine”.
+//Select “Back up automatically”
+//Select drive to back up automatically
