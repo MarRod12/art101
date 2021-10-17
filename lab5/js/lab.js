5d3f09f4d51999e9a6ad581086b8a78ef66315d6
MarRod12 <91565970+MarRod12@users.noreply.github.com>
2021-10-16 17:12:20 -0700

    lab 5 finished

---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+/*
+ * Author: Margarita Rodriguez
+ * Created: 16 October 2021
+ * License: Public Domain
+ *
+ */
+
+ //Define Variables
+ make = "Hyundai";
+ model = "Elantra";
+ color = "Grey";
+ year = 2018;
+
+ //calculate
+ age = 2021 - year;
+
+ //output
+
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " years<br>");
