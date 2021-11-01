e9e339e2046350fa23d35cd7d1793b25210fd932
MarRod12 <91565970+MarRod12@users.noreply.github.com>
2021-11-01 16:07:28 -0700

    added and printed two new elements

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -4,3 +4,25 @@
 *
 * License: Public Domain
 */
+
+let outputEl = document.getElementById('output');  //get the elements in the content div
+console.log(outputEl);  //print in console
+
+let new1El = document.createElement("p");  //create element p
+let linebreak = document.createElement("br");  //create line break element
+let linebreak2 = document.createElement("br"); //second line break
+
+let change1El = document.createTextNode("This is something new! ");  //change text of new element
+console.log(change1El);  //print in console
+
+outputEl.appendChild(change1El);   //add it to the results div
+outputEl.appendChild(linebreak);   //add linebreak
+outputEl.appendChild(linebreak2);  //add second line break
+
+let new2El = document.createElement("p");  //new element
+
+//create a variable to change the new2El
+let change2El = document.createTextNode("This is a second new element. :)");
+console.log(change2El);  //print in console
+
+outputEl.appendChild(change2El);   //add it to the results div
