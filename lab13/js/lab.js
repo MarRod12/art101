72881816bdf1f50424b452caa4a1a1b7853b801c
MarRod12 <91565970+MarRod12@users.noreply.github.com>
2021-11-18 17:43:17 -0800

    started lab 13 and got javascript to print in console

---
+++ lab13/js/lab.js
@@ -0,0 +1,31 @@
+/*
+* Author: Margarita Rodriguez
+* Created: 18 November 2021
+*
+* License: Public Domain
+*/
+
+function fizzbuzz(i) {
+  for (var i = 1; i < 201; i++) {             // loop from 1 to 200
+    if (i % 15 == 0) {
+      console.log("FizzBuzz");
+      }           // if divisble by 15
+    else if (i % 21 == 0) {
+      console.log("FizzBoom");
+      }          // if divisible by 21
+    else if (i % 35 == 0) {
+      console.log("BuzzBoom");
+      }          // if divisible by 3
+    else if (i % 3 == 0) {
+      console.log("Fizz");
+      }                 // if divisible by 3
+    else if (i % 5 == 0) {
+      console.log("Buzz");
+      }                 // if divisble by 5
+    else if (i % 7 == 0) {
+      console.log("Boom");
+      }                // if divisble by 7
+    }
+}
+
+console.log(fizzbuzz());
