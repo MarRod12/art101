194aa2c1b42275985a5479bdb20546245dbaf6ea
MarRod12 <91565970+MarRod12@users.noreply.github.com>
2021-11-20 15:24:46 -0800

    finished lab 13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -8,22 +8,27 @@
 function fizzbuzzboom() {
   for (var i = 1; i < 201; i++) {             // loop from 1 to 200
     if (i % 15 == 0) {
-      document.writeln("FizzBuzz");
-      console.log("FizzBuzz");
+      document.writeln("FizzBuzz ");
+      console.log("FizzBuzz ");
       }           // if divisble by 15
     else if (i % 21 == 0) {
+      document.writeln("FizzBoom ");
       console.log("FizzBoom");
       }          // if divisible by 21
     else if (i % 35 == 0) {
+      document.writeln("BuzzBoom ");
       console.log("BuzzBoom");
       }          // if divisible by 3
     else if (i % 3 == 0) {
+      document.writeln("Fizz ");
       console.log("Fizz");
       }                 // if divisible by 3
     else if (i % 5 == 0) {
+      document.writeln("Buzz ");
       console.log("Buzz");
       }                 // if divisble by 5
     else if (i % 7 == 0) {
+      document.writeln("Boom ");
       console.log("Boom");
       }                // if divisble by 7
     }
